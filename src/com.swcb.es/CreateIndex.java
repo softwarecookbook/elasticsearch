f58736b92bc5a27a51566c11ffb86951a6bbc43e
keithag <keithgutfreund@pobox.com>
2013-05-15 17:34:36 -0400

    Create CreateIndex.java

---
+++ src/com.swcb.es/CreateIndex.java
@@ -0,0 +1,44 @@
+package com.swcb.es;
+
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.indices.IndexAlreadyExistsException;
+import org.elasticsearch.indices.InvalidIndexNameException;
+
+public class CreateIndex {
+  
+	/**
+	 * Create indexName
+	 * @return true on success
+	 * @throws IndexAlreadyExistsException, InvalidIndexNameException, Exception
+	 */
+	public boolean createIndex(String indexName) {
+		boolean result = false;
+		
+		Client client = new TransportClient()
+		.addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
+
+		CreateIndexRequestBuilder createIndexRequestBuilder = 
+			client.admin().indices().prepareCreate(indexName);
+		try {
+			CreateIndexResponse cir = createIndexRequestBuilder.execute().actionGet();
+			result = cir.isAcknowledged();
+		} catch (IndexAlreadyExistsException e) {
+			e.printStackTrace();
+		} catch (InvalidIndexNameException e) {
+			e.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		CreateIndex ci = new CreateIndex();
+		boolean result = ci.createIndex("twittertest");
+		System.out.println("twittertest create " + (result ? "succeeded": "failed"));
+	}
+}
