7139e92e4489957fd88f09899b3f2df5610a4dc7
keithag <keithgutfreund@pobox.com>
2013-05-15 18:46:31 -0300

    Update CreateIndex.java

--- src/com.swcb.es/CreateIndex.java
+++ src/com.swcb.es/CreateIndex.java
@@ -9,36 +9,36 @@ import org.elasticsearch.indices.IndexAlreadyExistsException;
 import org.elasticsearch.indices.InvalidIndexNameException;
 
 public class CreateIndex {
-  
-	/**
-	 * Create indexName
-	 * @return true on success
-	 * @throws IndexAlreadyExistsException, InvalidIndexNameException, Exception
-	 */
-	public boolean createIndex(String indexName) {
-		boolean result = false;
-		
-		Client client = new TransportClient()
-		.addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
+    
+    /**
+     * Create indexName
+     * @return true on success
+     * @throws IndexAlreadyExistsException, InvalidIndexNameException, Exception
+     */
+    public boolean createIndex(String indexName) {
+        boolean result = false;
+        
+        Client client = new TransportClient()
+        .addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
 
-		CreateIndexRequestBuilder createIndexRequestBuilder = 
-			client.admin().indices().prepareCreate(indexName);
-		try {
-			CreateIndexResponse cir = createIndexRequestBuilder.execute().actionGet();
-			result = cir.isAcknowledged();
-		} catch (IndexAlreadyExistsException e) {
-			e.printStackTrace();
-		} catch (InvalidIndexNameException e) {
-			e.printStackTrace();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-	
-	public static void main(String[] args) {
-		CreateIndex ci = new CreateIndex();
-		boolean result = ci.createIndex("twittertest");
-		System.out.println("twittertest create " + (result ? "succeeded": "failed"));
-	}
+        CreateIndexRequestBuilder createIndexRequestBuilder = 
+            client.admin().indices().prepareCreate(indexName);
+        try {
+            CreateIndexResponse cir = createIndexRequestBuilder.execute().actionGet();
+            result = cir.isAcknowledged();
+        } catch (IndexAlreadyExistsException e) {
+            e.printStackTrace();
+        } catch (InvalidIndexNameException e) {
+            e.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        CreateIndex ci = new CreateIndex();
+        boolean result = ci.createIndex("twittertest");
+        System.out.println("twittertest create " + (result ? "succeeded": "failed"));
+    }
 }
