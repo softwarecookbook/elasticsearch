fb33cf403f59d906845b2570735c4c025d10ce8a
keithgutfreund <keithgutfreund@hotmail.com>
2013-06-04 23:50:50 -0300

    Create MatchQueryTest.java

---
+++ java/test/com/swcb/es/MatchQueryTest.java
@@ -0,0 +1,70 @@
+package com.swcb.es;
+
+import static org.junit.Assert.*;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.search.SearchHits;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MatchQueryTest {
+
+    static Client client;
+    
+    static final String INDEX_NAME = "indextest";
+    static final String INDEX_TYPE1 = "indextype1";
+    
+    static final String DOC1_TEXT = "my dog has fleas";
+    static final String DOC1 = "{\"f1\":\"" + DOC1_TEXT + "\"}";    
+    static final String DOCNUM1 = "1";
+
+    
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        client = ESClient.MakeTransportClient("localhost", 9300);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        client = null;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        DeleteIndex.delete(client, INDEX_NAME);
+        CreateIndex.create(client, INDEX_NAME);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        DeleteIndex.delete(client, INDEX_NAME);
+    }
+       
+
+    @Test
+    public final void testQueryStringObject() {
+        InsertDocument.insert(client, INDEX_NAME, INDEX_TYPE1, DOC1, DOCNUM1);
+        RefreshIndex.refresh(client, INDEX_NAME);
+
+        // This query will match using the default operator OR
+        MatchQueryBuilder mqb = MatchQuery.query("f1", "dog cats");
+        SearchResponse response = Search.search(client, mqb, new String[] {INDEX_NAME}, new String[] {INDEX_TYPE1});
+        SearchHits hits = response.getHits();
+        assertTrue(hits.getTotalHits() == 1);
+    }
+
+    @Test
+    public final void testQueryStringObjectBOOLEANS() {
+        
+        // This query will not match using the AND operator
+        MatchQueryBuilder mqb = MatchQuery.query("f1", "dog cats", MatchQuery.BOOLEANS.AND);
+        SearchResponse response = Search.search(client, mqb, new String[] {INDEX_NAME}, new String[] {INDEX_TYPE1});
+        SearchHits hits = response.getHits();
+        assertTrue(hits.getTotalHits() == 0);
+    }
+}
